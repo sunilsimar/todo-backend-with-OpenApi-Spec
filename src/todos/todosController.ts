a60f69bf4f4ae2ad21205cf9dd8f0181bba08e73
Sunil <sunisimar2002d@gmail.com>
2023-12-12 11:09:02 +0530

    created node client

--- src/todos/todosController.ts
+++ src/todos/todosController.ts
@@ -13,6 +13,9 @@ import { TodoCreatiionParams, TodoService } from "./todoService";
 
 @Route("todo")
 export class TodoController extends Controller {
+  /**
+   * This is controller for creating todos
+   */
   @Get("{todoId}")
   public async getTodo(@Path() todoId: string): Promise<Todo> {
     let todoService = new TodoService();
